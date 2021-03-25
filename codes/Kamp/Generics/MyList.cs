e7e059db43d5b4c16587b2d595abef34d09c33f1
Ali Atakay <aliatakay@gmail.com>
2021-03-25 16:33:10 +0300

    generics project added

---
+++ codes/Kamp/Generics/MyList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class MyList<Type>
+    {
+        Type[] array;
+
+        public MyList()
+        {
+            array = new Type[0];
+        }
+
+        public void Add(Type value)
+        {
+            Type[] temp = array;
+
+            array = new Type[array.Length + 1];
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                array[i] = temp[i];
+            }
+
+            array[array.Length - 1] = value;
+        }
+
+        public int Length => array.Length;
+
+        public Type[] Items => array;
+    }
+}
---
+++ codes/Kamp/Generics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> fruits = new MyList<string>();
+
+            fruits.Add("Elma");
+            fruits.Add("Armut");
+            fruits.Add("Çilek");
+
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                Console.WriteLine(fruits.Items[i]);
+            }
+        }
+    }
+}
