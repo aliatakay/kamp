be6c83e553cfcfc278e68e5c480c853f726bfd95
Ali Atakay <aliatakay@gmail.com>
2021-03-26 02:34:38 +0300

    OOP2 project added

---
+++ codes/Kamp/OOP2/CorporateCustomer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    public class CorporateCustomer : Customer
+    {
+        public string CompanyName { get; set; }
+        public string TaxNumber { get; set; }
+
+    }
+}
---
+++ codes/Kamp/OOP2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new IndividualCustomer
+            {
+                Id = 1,
+                FirstName = "Ali",
+                LastName = "Atakay"
+            };
+
+            Customer customer2 = new CorporateCustomer
+            {
+                Id = 2,
+                CompanyName = "Company",
+                TaxNumber = "110774"
+            };
+
+            CustomerManager manager = new CustomerManager();
+            manager.Add(customer1);
+            manager.Add(customer2);
+        }
+    }
+}
