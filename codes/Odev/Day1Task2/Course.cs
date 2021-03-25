9ad033a872fc49420bd79dd645d8bd6afc9f84fd
Ali Atakay <aliatakay@gmail.com>
2021-03-26 02:41:55 +0300

    homework of day 1 completed

---
+++ codes/Odev/Day1Task2/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day1Task2
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public int EducatorId { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ codes/Odev/Day1Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day1Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Course> courses = new List<Course>();
+
+            Course kurs1 = new Course
+            {
+                Id = 1,
+                EducatorId = 1,
+                Title = "C#",
+                Price = 0,
+                Description = "Sıfırdan C# Eğitimi"
+            };
+
+            Course kurs2 = new Course
+            {
+                Id = 2,
+                EducatorId = 1,
+                Title = "Java",
+                Price = 0,
+                Description = "Sıfırdan Java Eğitimi"
+            };
+
+            courses.Add(kurs1);
+            courses.Add(kurs2);
+
+            foreach (var kurs in courses)
+            {
+                Console.WriteLine(kurs.Description);
+            }
+        }
+    }
+}
