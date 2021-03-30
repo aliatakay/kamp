61974dd07ecfc85d5077c646a62fcee98fe25b24
Ali Atakay <aliatakay@gmail.com>
2021-03-30 22:20:45 +0300

    OOP3 project added

---
+++ codes/Kamp/OOP3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            konutKrediManager.Hesapla();
+
+            IKrediManager emlakKrediManager = new EmlakKrediManager();
+            emlakKrediManager.Hesapla();
+
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            ihtiyacKrediManager.Hesapla();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(konutKrediManager);
+            basvuruManager.BasvuruYap(emlakKrediManager);
+
+        }
+    }
+}
