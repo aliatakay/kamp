48053e30dc1befd8b11673f6ccec7a6e67f6fc3a
Ali Atakay <aliatakay@gmail.com>
2021-03-31 17:23:19 +0300

    homework of day 4 completed

---
+++ codes/Odev/Day4Task5/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4Task5
+{
+    public class MyDictionary<TKey, TVal>
+    {
+        TKey[] keys;
+        TVal[] values;
+
+        TKey[] tempKeys;
+        TVal[] tempValues;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TVal[0];
+        }
+
+        public void Add(TKey key, TVal value)
+        {
+            tempKeys = keys;
+            tempValues = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TVal[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[^1] = key;
+            values[^1] = value;
+        }
+
+        public int Length => keys.Length;
+    }
+}
---
+++ codes/Odev/Day4Task5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day4Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> students = new Dictionary<int, string>();
+
+            students.Add(145, "Ali Atakay");
+            students.Add(178, "Ahmet Demir");
+            students.Add(221, "Zeynep Deniz");
+
+            foreach (var student in students)
+            {
+                Console.WriteLine("Student Number: " + student.Key);
+                Console.WriteLine("Student Name: " + student.Value);
+            }
+
+            MyDictionary<int, string> teachers = new MyDictionary<int, string>();
+
+            teachers.Add(1, "Ali Atakay");
+            teachers.Add(2, "Ahmet Demir");
+            teachers.Add(3, "Zeynep Deniz");
+
+            Console.WriteLine(teachers.Length);
+
+        }
+    }
+}
